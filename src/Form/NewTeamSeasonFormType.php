1063a4c2f4152586d0b8852998c9364bf1903569
Florian Messu <florian.messu@gmail.com>
2022-12-30 19:05:55 +0100

    new season form input type

--- src/Form/NewTeamSeasonFormType.php
+++ src/Form/NewTeamSeasonFormType.php
@@ -4,8 +4,8 @@ namespace App\Form;
 
 use App\Entity\TeamSeason;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,24 +14,9 @@ class NewTeamSeasonFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', ChoiceType::class, [
+            ->add('name', TextType::class, [
                 'label' => 'Année',
-                'required' => true,
-                'choices' => [
-                    '2020-2021' => '2020-2021',
-                    '2021-2022' => '2021-2022',
-                    '2022-2023' => '2022-2023',
-                    '2023-2024' => '2023-2024',
-                    '2024-2025' => '2024-2025',
-                    '2025-2026' => '2025-2026',
-                    '2026-2027' => '2026-2027',
-                    '2027-2028' => '2027-2028',
-                    '2028-2029' => '2028-2029',
-                    '2029-2030' => '2029-2030',
-                    '2030-2031' => '2030-2031',
-                    '2031-2032' => '2031-2032',
-                    '2032-2033' => '2032-2033',
-                ]
+                'required' => true
             ])
             ->add('save', SubmitType::class, [
                 'label' => 'Valider'
